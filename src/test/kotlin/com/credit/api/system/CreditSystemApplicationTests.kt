1a7d90060af7349664435e0450e76b486ded6b86
Bruno Paulin <bruno.p-@hotmail.com>
2024-02-12 21:50:15 -0300

    first

---
+++ src/test/kotlin/com/credit/api/system/CreditSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.credit.api.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
